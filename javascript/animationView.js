af49cdfaff57873ff0cd9a42b9b0cc177d4e63f8
Vinicius <viniciusfelicianomello@hotmail.com>
2024-06-01 14:59:22 -0300

    Seção sobre completa, com animações e responsividade

--- javascript/animationView.js
+++ javascript/animationView.js
@@ -1,39 +1,24 @@
-// Função para animção apenas no viewport
-function animateView() {
-    var animate = document.querySelectorAll('.viewScreen');
+// Função para verificar se o elemento está visível na tela
+function isElementInViewport(el, margin) {
+    var rect = el.getBoundingClientRect();
+    return (
+        rect.bottom >= - margin &&
+        rect.right >= 0 &&
+        rect.top <= (window.innerHeight || document.documentElement.clientHeight) &&
+        rect.left <= (window.innerWidth || document.documentElement.clientWidth)
+    );
+}
 
-    animate.forEach(function(cont) {
-        // Verifica se pelo menos parte do view está visível e se ainda não foi animado
-        if (isPartiallyInViewport(cont) && !cont.classList.contains('animado')) {
-            var Destino = parseInt(cont.innerText);
-            var inicio = 0;
-            var Acrescimo = Math.ceil(Destino / 100); // Define o incremento
-
-            // Função para atualizar a animação
-            function atualizarAnimação() {
-                inicio += Acrescimo;
-                if (inicio >= Destino) {
-                    cont.innerText = Destino;
-                } else {
-                    cont.innerText = inicio;
-                    setTimeout(atualizarAnimação, 20); // Atualiza a cada 130 milissegundos
-                }
-            }
-
-            atualizarAnimação();
-
-            // Marca o elemento como animado para que não seja reiniciado
-            cont.classList.add('animado');
+// Função para lidar com o evento de rolagem
+function handleScroll() {
+    var animationElements = document.querySelectorAll('.viewport');
+    animationElements.forEach(function(element) {
+        if (isElementInViewport(element, 100)) {
+            element.classList.add('visible');
+            } else {
         }
     });
 }
 
-// Adiciona um ouvinte de evento de rolagem à página para garantir que a animação seja iniciada mesmo se a seção estiver visível após o carregamento inicial
-window.addEventListener('scroll', function() {
-    animateView();
-});
-
-// Chama a função quando a página é carregada
-window.addEventListener('load', function() {
-    animateView();
-});
+window.addEventListener('scroll', handleScroll);
+handleScroll();
