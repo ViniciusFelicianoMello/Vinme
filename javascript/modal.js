2a23796539411dea86f45d4095b6c651f6bdd81c
Vinicius <viniciusfelicianomello@hotmail.com>
2024-06-01 19:52:34 -0300

    Seção serviços quase completa, animaçoes feitas

---
+++ javascript/modal.js
@@ -0,0 +1,30 @@
+const modalView = document.querySelectorAll(".modal"),
+      modalMais = document.querySelectorAll(".vermais"),
+      modalClose = document.querySelectorAll(".modal_close");
+
+        // Função para abrir o modal
+        function abrirModal(index) {
+            modalView[index].classList.add("active-modal");
+        }
+
+        // Função para fechar o modal
+        function fecharModal() {
+            modalView.forEach(modal => {
+                modal.classList.remove("active-modal");
+            });
+        }
+
+        // Adiciona o ouvinte de evento para abrir o modal
+        modalMais.forEach((modal, index) => {
+            modal.addEventListener("click", () => {
+                abrirModal(index);
+            });
+        });
+
+        // Adiciona o ouvinte de evento para fechar o modal
+        modalClose.forEach(modal => {
+            modal.addEventListener("click", (event) => {
+                event.stopPropagation();
+                fecharModal();
+            });
+        });
