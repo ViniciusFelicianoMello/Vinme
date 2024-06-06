5a30364482d4fc800c9dd2abd46120247aecf705
Vinicius <viniciusfelicianomello@hotmail.com>
2024-06-06 17:00:52 -0300

    adicinando tabs

---
+++ javascript/tabs.js
@@ -0,0 +1,21 @@
+document.addEventListener("DOMContentLoaded", function() {
+    const tabButtons = document.querySelectorAll(".tab-button");
+    const tabContents = document.querySelectorAll(".tab-content");
+
+    // para cada botão das abas, obtem o valor do atributo do click
+    tabButtons.forEach(button => {
+        button.addEventListener("click", () => {
+            const tabId = button.getAttribute("data-tab");
+
+            tabButtons.forEach(btn => btn.classList.remove("active"));
+            button.classList.add("active");
+
+            tabContents.forEach(content => {
+                content.classList.remove("active");
+                if (content.id === tabId) {
+                    content.classList.add("active");
+                }
+            });
+        });
+    });
+});
