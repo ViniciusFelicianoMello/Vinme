c9cb7842c2c75b5421082cd8e61a31716315840c
Vinicius <viniciusfelicianomello@hotmail.com>
2024-07-15 15:43:40 -0300

    preloader

---
+++ setup/static/javascript/preLoader.js
@@ -0,0 +1,4 @@
+window.addEventListener('load', function() {
+    document.getElementById('preloader').style.display = 'none';
+    document.getElementById('main-content').style.display = 'block';
+});
